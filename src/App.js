51e8d6b652b1fa9c32f6956be5b10b6aba6e7647
Notahotdog-Umar Moiz <umarmoiz@gmail.com>
2021-03-21 06:40:08 +0800

    integrated JsonData with frontend, differentiate JSON Data to corresponding arrays

--- src/App.js
+++ src/App.js
@@ -14,12 +14,49 @@ class App extends Component {
       })
     );
 
-    jsonDataLoad((err, jsonData) => this.setState({ jsonData }));
+    //Upload Json Data
+    // updateJsonData = (err,jsonObj) => {
+    //   var obj = JSON.parse(jsonObj);
+    //   var date = [];
+    //   var yhat = [];
+    //   var lower = [];
+    //   var upper = [];
+
+    //   for (var i in obj) date.push["ds"];
+
+    //   this.setState({ date }); // set State the Date
+    // };
+
+    // this.updateJsonData = this.updateJsonData.bind(this);
+
+    //jsonDataLoad((err, jsonData) => this.setState({ jsonData }));
+    jsonDataLoad((err, jsonData) => {
+      var obj = JSON.parse(jsonData);
+      var date = [];
+      var yhat = [];
+      var lower = [];
+      var upper = [];
+
+      for (var i in obj) {
+        // console.log(obj[i].ds);
+        date.push(obj[i].ds);
+        yhat.push(obj[i].yhat);
+        lower.push(obj[i].lower);
+        upper.push(obj[i].upper);
+      }
+
+      this.setState({ date });
+      this.setState({ yhat });
+      this.setState({ lower });
+      this.setState({ upper });
+      // this.setState({ jsonData });
+    });
   }
 
   state = {
     timestamp: "no timestamp yet",
     jsonData: "Retrieving Data",
+    date: [],
   };
 
   render() {
@@ -30,6 +67,10 @@ class App extends Component {
         </p>
         Json Data Load Value : {this.state.jsonData}
         <p>Json Data Type : {typeof this.state.jsonData}</p>
+        Date array : {this.state.date}
+        Date array format {typeof this.state.date}
+        yHat array : {this.state.yhat}
+        yhat array format {typeof this.state.yhat}
       </div>
     );
   }
